712019d02fe76e7168e6570ae1b5e4157e0d7605
abWasHere <audrey.belson@gmail.com>
2020-06-23 18:40:38 +0200

    deploys canvas with play on it, improves Bots class

--- javascript/app.js
+++ javascript/app.js
@@ -1,88 +1,136 @@
 /* ---------- DOM GAMEPLAY/INTERFACE */
 const playerName = document.getElementById('landing-player-name');
 const gotItBtn = document.getElementById('landing-button-ok');
-const gifReaction = document.getElementById('gif-reaction');
 const level1Btn = document.getElementById('level1');
 const level2Btn = document.getElementById('level2');
 const level3Btn = document.getElementById('level3');
+const resetBtn = document.getElementById('reset-button');
 
 /* ---------- DOM PLAYERS */
 const playerPic = document.getElementById('picture-player');
 const botPic = document.getElementById('picture-bot');
 const playerAnswers = document.getElementById('player-answers');
 const botSpeech = document.getElementById('bot-speech');
+const gifReaction = document.getElementById('gif-reaction');
 
+/* ---------- CANVAS */
 
-/* ---------- PLAYGROUND EVENTS */
+// var canvasContainer = document.getElementById('game-interface');
+var canvas = document.getElementById('canvas');
+canvas.width = 780;
+canvas.height = 415;
+// la propriété CLIENTWIDTH NE FONCTIONNE PAS ...
+var ctx = canvas.getContext('2d');
+console.log(canvas.height);
+
+/* ---------- GLOBAL FUNCTIONS */
+
+function getRandomInt(nb) {
+   return Math.floor(Math.random() * Math.floor(nb));
+}
 
 function gifReactions(gif) {
-    gifReaction.setAttribute('src', `${gif}`);
+   gifReaction.setAttribute('src', `${gif}`);
 }
- 
-/* ---------- END GAME EVENTS */
- 
- function getRandomInt(nb) {
-    return Math.floor(Math.random() * Math.floor(nb));
- }
- 
- function closePlayground(endgame, newgif) {
-    var playground = document.getElementById('game-interface');
-    playground.innerHTML = `<p class="flex" style="flex-direction: column; align-items: center"> ${endgame} </p>`;
- 
-    gifReaction(newgif);
- }
- 
- function gameOver(reason) {
-    const gifGetOut = [
-       `../images/reactions-gif/gif-get-out.gif`,
-       `../images/reactions-gif/gif-fired.gif`,
-    ];
-    const gifBreakdown = [
-       `../images/reactions-gif/gif-nailed-it-haaaa.gif`,
-       `../images/reactions-gif/gif-nailed-it-crazy.gif`,
-    ];
- 
-    switch (reason) {
-       case 'too much stress':
-          closePlayground(
-             'GAME OVER - YOU HAD A NERVOUS BREAKDOWN...',
-             gifBreakdown[getRandomInt(gifGetOut.length)]);
-          break;
- 
-       case 'no time left':
-          closePlayground(
-             'GAME OVER - YOU NEED TO IMPROVE TIME MANAGEMENT !',
-             gifGetOut[getRandomInt(gifGetOut.length)]
-          );
-          break;
- 
-       default:
-          console.log('BUG');
-    }
- }
- 
- function winGame() {
-    console.log('YOU WIN');
-    const gifWin = [
-       `../images/reactions-gif/tiffany-dance-wine-home.gif`,
-       `../images/reactions-gif/tiffany-made-it.gif`,
-       `../images/reactions-gif/gif-proud.gif`,
-       `../images/reactions-gif/gif-nailed-it-done.gif`,
-    ];
- 
-    closePlayground('NAILED IT !!!!!!', gifWin.getRandomInt(gifWin.length));
- }
+
+function closePlayground(endgame, newgif) {
+   var playground = document.getElementById('game-interface');
+   playground.innerHTML = `<p class="flex" style="flex-direction: column; align-items: center"> ${endgame} </p>`;
+
+   gifReaction(newgif);
+}
+
+function gameOver(reason) {
+   const gifGetOut = [
+      `../images/reactions-gif/gif-get-out.gif`,
+      `../images/reactions-gif/gif-fired.gif`,
+   ];
+   const gifBreakdown = [
+      `../images/reactions-gif/gif-nailed-it-haaaa.gif`,
+      `../images/reactions-gif/gif-nailed-it-crazy.gif`,
+   ];
+
+   switch (reason) {
+      case 'too much stress':
+         closePlayground(
+            'GAME OVER - YOU HAD A NERVOUS BREAKDOWN...',
+            gifBreakdown[getRandomInt(gifGetOut.length)]
+         );
+         break;
+
+      case 'no time left':
+         closePlayground(
+            'GAME OVER - YOU NEED TO IMPROVE TIME MANAGEMENT !',
+            gifGetOut[getRandomInt(gifGetOut.length)]
+         );
+         break;
+
+      default:
+         console.log('BUG');
+   }
+}
+
+function winGame() {
+   console.log('YOU WIN');
+   const gifWin = [
+      `../images/reactions-gif/tiffany-dance-wine-home.gif`,
+      `../images/reactions-gif/tiffany-made-it.gif`,
+      `../images/reactions-gif/gif-proud.gif`,
+      `../images/reactions-gif/gif-nailed-it-done.gif`,
+   ];
+
+   closePlayground('NAILED IT !!!!!!', gifWin.getRandomInt(gifWin.length));
+}
+
+/* ------------------- CANVAS - DEPLACEMENTS */
+
+function loadAvatar(person) {
+   var img = new Image(); // Crée un nouvel élément Image
+   ctx.beginPath();
+   img.onload = function () {
+      ctx.drawImage(img, person.x, person.y, 30, 40);
+    };
+    ctx.clearRect(0, 0, innerWidth, innerHeight);
+   img.src = `${person.avatar}`;
+}
 
 /* ---------- PLAYERS */
 
 const player = {
-   name: 'Tiffany',
    time: 100,
    stress: 10,
    picture: '../images/pic-tiffany-normal-100.png',
-   avatar: '../images/avatar-player-tiffany-50.jpg.png',
+   avatar: '../images/avatar-player-tiffany-50.jpg',
    x: 10,
    y: 10,
+   dx: 4, // pas de vélocité en abscisses
+   dy: 4, // pas de vélocité en ordonnées
+
+   loseTime(usage) {
+      this.time -= usage;
+      if (this.time <= 100) {
+         return gameOver('no time left');
+      } else if (this.time > 100) {
+         return (this.time = 100);
+      } else if (this.time < 15) {
+         playerAnswers.innerHTML = `<p class="talks">Oh sh*t !</p>`;
+      } else {
+         console.log(this.time);
+         return this.time;
+      }
+   },
+
+   manageStress(effect) {
+      this.stress += effect;
+      if (this.stress > 100) {
+         return gameOver('to much stress');
+      } else if (this.stress < 0) {
+         playerAnswers.innerHTML = `<p class="talks">Living my best life !</p>`;
+         return (this.stress = 0);
+      } else {
+         return this.stress;
+      }
+   },
 
    changePicture() {
       if (this.stress <= 10) {
@@ -128,48 +176,119 @@ const player = {
       }, 5500);
    },
 
-   manageStress(effect) {
-      this.stress += effect;
-      if (this.stress > 100) {
-         return gameOver('to much stress');
-      } else if (this.stress < 0) {
-         playerAnswers.innerHTML = `<p class="talks">Living my best life !</p>`;
-         return (this.stress = 0);
-      } else {
-         console.log(this.stress);
-         return this.stress;
+   move(event) {
+      loadAvatar(player);
+      switch (event.code) {
+         case 'KeyW':
+            if (player.y >= player.dy) {
+               player.y -= player.dy;
+            }
+            console.log('player has moved UP');
+            break;
+         case 'KeyS':
+            if (player.y < canvas.height - player.dy) player.y += player.dy;
+            console.log('player has moved DOWN');
+            break;
+         case 'KeyA':
+            if (player.x >= player.dx) player.x -= player.dx;
+            console.log('player has moved LEFT');
+            break;
+         case 'KeyD':
+            if (player.x < canvas.width - player.dx) player.x += player.dx;
+            console.log('player has moved RIGHT');
+            break;
+         default:
+            alert('please type a valid key : Z - Q - S - D');
       }
-   },
-
-   loseTime(usage) {
-      this.time -= usage;
-      if (this.time <= 100) {
-         return gameOver('no time left');
-      } else if (this.time > 100) {
-         return (this.time = 100);
-      } else if (this.time < 15) {
-         playerAnswers.innerHTML = `<p class="talks">Oh sh*t !</p>`;
-      } else {
-         console.log(this.time);
-         return this.time;
-      }
-   },
-
-   // modifier les unités si utilisation du CANVAS
-   moveUp() {
-      if (this.player.y > 0) this.player.y -= dy;
-   },
-   moveDown() {
-      if (this.player.y > canvas.innerHeight) this.player.y += dy;
-   },
-   moveLeft() {
-      if (this.player.x > 0) this.player.y -= dx;
-   },
-   moveUp() {
-      if (this.player.x < canvas.innerWidth) this.player.y += dx;
+      console.log(`player position is x : ${player.x}, y : ${player.y}`);
    },
 };
 
+class Bots {
+    constructor(time, stress, x, y, dx, dy, pictures, avatars) {
+       this.time = time;
+       this.stress = stress;
+       this.x = x;
+       this.y = y;
+       this.pictures = pictures;
+       this.avatars = avatars;
+       this.dx = dx;
+       this.dy = dy;
+    }
+    changePicture() {
+       var randomPic = this.pictures[getRandomInt(this.pictures.length)];
+       botPic.setAttribute('src', `${randomPic}`);
+       botPic.removeAttribute('style');
+    }
+    move() {
+       // change the coordinates
+       if (this.x + this.dx > innerWidth || this.x - this.dx < 0) {
+          this.dx = -this.dx; // on change de sens sur l'axe des x
+       }
+       if (y + this.dy > innerHeight || this.y - this.dy < 0) {
+          this.dy = -this.dy; // on change de sens sur l'axe des y
+       }
+       // incrémente les coordonnées des bots
+       this.x += this.dx;
+       this.y += this.dy;
+ 
+       loadAvatar(this);
+    }
+ 
+    useTime() {
+       return this.time;
+    }
+    giveStress() {
+       return this.stress;
+    }
+ }
+
+ class ExLovers extends Bots {
+    constructor(time, stress, x, y, dx, dy) {
+       super(time, stress, x, y, dx, dy);
+       super.useTime();
+       super.giveStress();
+       super.move();
+       super.changePicture();
+       this.pictures = [
+          '../images/bots-pictures/pic-bot-ex-drake-100.jpg',
+          '../images/bots-pictures/pic-bot-ex-laverne-100.jpg',
+          '../images/bots-pictures/pic-bot-ex-laurence-100.jpg',
+          '../images/bots-pictures/pic-bot-ex-meghan-100.jpg',
+       ];
+       this.avatars = [
+          '../images/bots-pictures/avatar-ex0.png',
+          '../images/bots-pictures/avatar-ex1.png',
+          '../images/bots-pictures/avatar-ex2.png',
+          '../images/bots-pictures/avatar-ex3.png',
+          '../images/bots-pictures/avatar-ex4.png',
+       ];
+       this.name = 'ex';
+    }
+ 
+    talk() {
+       var firstTalk = "Girl! It's been a long time...";
+ 
+       var talks = [
+          'I think I made a mistake!',
+          "I'm married. I have 2 kids and a dog now.",
+          "I've changed you know...",
+       ];
+ 
+       botSpeech.innerHTML = `<p class="talks">${firstTalk}</p>`;
+ 
+       setTimeout(() => {
+          botSpeech.innerHTML += `<p class='talks'> ${
+             talks[getRandomInt(talks.length)]
+          }`;
+       }, 4000);
+ 
+       player.answerTo('ex');
+    }
+ }
+
+
+/* ---------- CANVAS */
 
 /* ---------- LANDING PAGE EVENTS */
 
@@ -181,23 +300,22 @@ function definePlayerName(event) {
 }
 
 function setDifficulty(event) {
-   switch (event.target.id) {
-      case 'level1':
-         console.log(event.target.id, event.target.textContent);
-         generateBots(ex, 5);
-         generateBots(mil, 1);
-         break;
+   var levelDisplay = document.getElementById('level-display');
+   levelDisplay.innerText = `${event.target.textContent}`;
 
+   switch (event.target.id) {
       case 'level2':
-         console.log(event.target.id, event.target.textContent);
          generateBots(ex, 8);
          generateBots(mil, 1);
          break;
       case 'level3':
-         console.log(event.target.id, event.target.textContent);
          generateBots(ex, 10);
          generateBots(mil, 2);
          break;
+      default:
+         //LEVEL 1
+         generateBots(ex, 5);
+         generateBots(mil, 1);
    }
 }
 
@@ -222,7 +340,6 @@ function displayStress() {
    } else {
       stressLvl.innerHTML = `${player.stress}`;
    }
-   console.log('stress level : ' + stressLvl.innerText);
 }
 
 function startGame() {
@@ -237,8 +354,16 @@ function startGame() {
 
    chronometerOn();
    displayStress();
+
+   loadAvatar(player);
+
+   document.onkeypress = player.move;
 }
 
+function resetGame() {
+   //REFRESH THE PAGE
+   document.location.reload(true);
+}
 
 /* ---------- LISTENERS */
 
@@ -247,3 +372,4 @@ gotItBtn.addEventListener('click', startGame);
 level1Btn.addEventListener('click', setDifficulty);
 level2Btn.addEventListener('click', setDifficulty);
 level3Btn.addEventListener('click', setDifficulty);
+resetBtn.addEventListener('click', resetGame);
--- javascript/players.js
+++ javascript/players.js
@@ -1,141 +1,3 @@
-/* ------------------- DOM ELEMENTS */
-
-const playerPic = document.getElementById('picture-player');
-const botPic = document.getElementById('picture-bot');
-const playerAnswers = document.getElementById('player-answers');
-const botSpeech = document.getElementById('bot-speech');
-
-
-/* ------------------- SHARED FUNCTIONS */
-
-function getRandomInt(nb) {
-   return Math.floor(Math.random() * Math.floor(nb));
-}
-// console.log(getRandomInt(4)) // returns an integer between 0 and 3
-
-/* ------------------- VITESSE DEPLACEMENT PLAYER */
-
-var dx = 5; // pas de vélocité en abscisses
-var dy = 5; // pas de vélocité en ordonnées
-
-/* ------------------- PLAYER OBJECT */
-
-const player = {
-   name: 'Tiffany',
-   time: 100,
-   stress: 10,
-   picture: '../images/pic-tiffany-normal-100.png',
-   avatar: '../images/avatar-player-tiffany-50.jpg.png',
-   x: 10,
-   y: 10,
-
-   changePicture() {
-      if (this.stress <= 10) {
-         this.picture = '../images/pic-tiffany-normal-100.png';
-      } else if (this.stress > 10 && this.stress <= 40) {
-         this.picture = '../images/pic-tiffany-mad-100.jpg';
-      } else if (this.stress > 40 && this.stress <= 80) {
-         this.picture = '../images/pic-tiffany-mad-100.jpg';
-      } else this.picture = '../images/pic-tiffany-escaping-100x150.jpg.jpg';
-
-      playerPicture.setAttribute('src', `${this.picture}`);
-   },
-
-   answerTo(bot) {
-      const firstAnswer = 'Heeeeeey!';
-
-      if (bot === 'ex') {
-         var answers = [
-            `I don't have time for that !`,
-            `Hmmm... I'm kind of in a rush...`,
-            `It was  nice to see ya! Bye!`,
-         ];
-      } else if (bot === 'motherInLaw') {
-         var answers = [
-            `I don't know what you're talking about ^^`,
-            `Oh! That's my bus there ! Gotta go !`,
-            `See you on Sunday ?`,
-         ];
-      } else if (bot === 'obstacles') {
-         var answers = [
-            `Oh! That's my bus there ! Gotta go !`,
-            `Sorry... Me no speak english...`,
-            `I'm late, ok ?!!`,
-         ];
-      }
-
-      playerAnswers.innerHTML = `<p class="talks">${firstAnswer}</p>`;
-
-      setTimeout(() => {
-         playerAnswers.innerHTML += `<p class="talks">
-         ${answers[getRandomInt(answers.length)]}
-      </p>`;
-      }, 5500);
-   },
-
-   manageStress(effect) {
-      this.stress += effect;
-      if (this.stress > 100) {
-         return gameOver('to much stress');
-      } else if (this.stress < 0) {
-         return (this.stress = 0);
-      } else {
-         console.log(this.stress);
-         return this.stress
-      }
-   },
-
-   loseTime(usage) {
-      this.time -= usage;
-      if (this.time <= 100) {
-         return gameOver('no time left');
-      } else if (this.time > 100) {
-         return (this.time = 100);
-      } else {
-         console.log(this.time);
-         return this.time
-      }
-   },
-
-   // modifier les unités si utilisation du CANVAS
-   moveUp() {
-      if (this.player.y > 0) this.player.y -= dy;
-   },
-   moveDown() {
-      if (this.player.y > canvas.innerHeight) this.player.y += dy;
-   },
-   moveLeft() {
-      if (this.player.x > 0) this.player.y -= dx;
-   },
-   moveUp() {
-      if (this.player.x < canvas.innerWidth) this.player.y += dx;
-   },
-};
-
-class Bots {
-   constructor(time, stress, x, y) {
-      this.avatar = 'avatar';
-      this.pictures = 'pictures';
-      this.time = time;
-      this.stress = stress;
-      this.x = x; // position x
-      this.y = y; // position y
-   }
-   changePicture() {
-      var randomPic = this.pictures[getRandomInt(this.pictures.length)];
-      botPic.setAttribute('src', `${randomPic}`);
-      botPic.removeAttribute('style');
-   }
-   move() {
-      return `${this.name} has moved`; // A CHANGER !!!
-   }
-   useTime() {
-      return this.time;
-   }
-   giveStress() {
-      return this.stress;
-   }
-}
 
 class Places extends Bots {
    constructor(name, logo, time, stress, x, y) {
@@ -147,53 +9,32 @@ class Places extends Bots {
    }
 }
 
-class ExLovers extends Bots {
-   constructor(time, stress, x, y) {
-      super(time, stress, x, y);
-      this.pictures = [
-         '../images/bots-pictures/pic-bot-ex-drake-100.jpg',
-         '../images/bots-pictures/pic-bot-ex-laverne-100.jpg',
-         '../images/bots-pictures/pic-bot-ex-laurence-100.jpg',
-         '../images/bots-pictures/pic-bot-ex-meghan-100.jpg',
-      ];
-      this.avatars = [
-         '../images/bots-pictures/avatar-ex0.png',
-         '../images/bots-pictures/avatar-ex1.png',
-         '../images/bots-pictures/avatar-ex2.png',
-         '../images/bots-pictures/avatar-ex3.png',
-         '../images/bots-pictures/avatar-ex4.png',
-      ];
-      this.name = 'ex';
-      super.move();
-      super.changePicture();
-      super.giveStress();
-      super.useTime();
-   }
+const places = [
+   {
+      name: 'starbucks',
+      logo: 'https://img.icons8.com/color/48/000000/starbucks.png',
+   },
+   {
+      name: 'Shopping Mall',
+      logo: 'https://img.icons8.com/doodle/96/000000/shopping-bag--v1.png',
+   },
+   { name: 'university', logo: '' },
+   { name: 'office', logo: '' },
+   {
+      name: 'home',
+      logo: 'https://img.icons8.com/doodle/96/000000/cottage--v1.png',
+   },
+];
 
-   talk() {
-      var firstTalk = "Girl! It's been a long time...";
 
-      var talks = [
-         'I think I made a mistake!',
-         "I'm married. I have 2 kids and a dog now.",
-         "I've changed you know...",
-      ];
-
-      botSpeech.innerHTML = `<p class="talks">${firstTalk}</p>`;
-
-      setTimeout(() => {
-         botSpeech.innerHTML += `<p class='talks'> ${
-            talks[getRandomInt(talks.length)]
-         }`;
-      }, 4000);
-
-      player.answerTo('ex');
-   }
-}
 
 class MotherInLaw extends Bots {
    constructor(time, stress, x, y) {
       super(time, stress, x, y);
+      super.useTime();
+      super.giveStress();
+      super.move();
+      super.changePicture();
       this.pictures = [
          `../images/bots-pictures/pic-bot-madea-1-100.jpg`,
          `../images/bots-pictures/pic-bot-madea-2-100.jpg`,
@@ -201,10 +42,6 @@ class MotherInLaw extends Bots {
       ];
       this.avatar = `../images/bots-pictures/avatar-mil-madea.png`;
       this.name = 'motherInLaw';
-      super.move();
-      super.giveStress();
-      super.useTime();
-      super.changePicture();
    }
 
    talk() {
@@ -229,6 +66,7 @@ class MotherInLaw extends Bots {
    }
 }
 
+/* 
 function generateBots(botType, amount) {
    let i = 0;
 
@@ -246,7 +84,7 @@ function generatePlaces(placeType) {
 
    }
 }
-
+ */
 
 /* ---------------------------------------------------- */
 /* ----------------------- TESTS ---------------------- */
@@ -262,21 +100,3 @@ function generatePlaces(placeType) {
 // console.log (exBoyfriend)
 
 // console.log(player.manageStress(20));
-
-const places = [
-   {
-      name: 'starbucks',
-      logo: 'https://img.icons8.com/color/48/000000/starbucks.png',
-   },
-   {
-      name: 'Shopping Mall',
-      logo: 'https://img.icons8.com/doodle/96/000000/shopping-bag--v1.png',
-   },
-   { name: 'university', logo: '' },
-   { name: 'office', logo: '' },
-   {
-      name: 'home',
-      logo: 'https://img.icons8.com/doodle/96/000000/cottage--v1.png',
-   },
-];
-
