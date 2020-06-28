1ebedccbf21496d421d1665fce8232424b677e82
abWasHere <audrey.belson@gmail.com>
2020-06-28 17:41:55 +0200

    adds random coordinates for objectives +  attempt to lower sound volume

--- javascript/app.js
+++ javascript/app.js
@@ -536,16 +536,16 @@ function animate(botCollection) {
 
 const university = {
    x: 600,
-   y: 60,
+   y: getRandomInt(canvas.height - 60),
    avatar: `./images/places/university.png`,
 };
 const office = {
    x: 30,
-   y: 270,
+   y: getRandomInt(canvas.height - 60),
    avatar: `./images/places/office2.png`,
 };
 const home = {
-   x: 270,
+   x: getRandomInt(canvas.width - 60),
    y: 20,
    avatar: `./images/places/home3.png`,
 };
@@ -654,6 +654,9 @@ function startGame() {
 
    // FIRST START OF THE CHRONO
    chronometer.startChrono(player);
+
+   // VOLUME
+   sound.volume = 0.10;
 }
 
 function resetGame() {
